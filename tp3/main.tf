7a49d30828d8122c15313e0b4313a369ac212ec1
ItsGibbs <thepapeyeku@gmail.com>
2023-04-04 16:40:18 +0200

    Update main.tf

--- tp3/main.tf
+++ tp3/main.tf
@@ -55,6 +55,18 @@ resource "azurerm_network_interface" "nic-b3-node2" {
   }
 }
 
+resource "azurerm_network_interface" "nic-b3-cloudinit" {
+  name                = "nic-b3-cloudinit"
+  location            = azurerm_resource_group.rg-b3-nodes.location
+  resource_group_name = azurerm_resource_group.rg-b3-nodes.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.s-b3-nodes.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
 resource "azurerm_public_ip" "ip-b3-node1" {
   name                = "ip-b3-node1"
   resource_group_name = azurerm_resource_group.rg-b3-nodes.name
@@ -98,6 +110,34 @@ resource "azurerm_linux_virtual_machine" "vm-b3-node2" {
   admin_username      = "gibbs"
   network_interface_ids = [
     azurerm_network_interface.nic-b3-node2.id,
+  ]
+
+  admin_ssh_key {
+    username   = "gibbs"
+    public_key = file("id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Debian"
+    offer     = "debian-10"
+    sku       = "10"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm-b3-cloudinit" {
+  name                = "b3-cloudinit"
+  resource_group_name = azurerm_resource_group.rg-b3-nodes.name
+  location            = azurerm_resource_group.rg-b3-nodes.location
+  size                = "Standard_B1s"
+  admin_username      = "gibbs"
+  network_interface_ids = [
+    azurerm_network_interface.nic-b3-cloudinit.id,
   ]
 
   admin_ssh_key {
