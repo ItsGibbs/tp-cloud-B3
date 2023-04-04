425675859ccf3429c6bc1b01a5b134bf74f95181
ItsGibbs <thepapeyeku@gmail.com>
2023-04-04 15:38:03 +0200

    add readme + updates

--- tp3/main.tf
+++ tp3/main.tf
@@ -11,45 +11,65 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg-b3-vm1" {
-  name     = "b3-vm1"
+resource "azurerm_resource_group" "rg-b3-nodes" {
+  name     = "b3-nodes"
   location = "eastus"
 }
 
-resource "azurerm_virtual_network" "vn-b3-vm1" {
-  name                = "b3-vm1"
+resource "azurerm_virtual_network" "vn-b3-nodes" {
+  name                = "b3-nodes"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg-b3-vm1.location
-  resource_group_name = azurerm_resource_group.rg-b3-vm1.name
+  location            = azurerm_resource_group.rg-b3-nodes.location
+  resource_group_name = azurerm_resource_group.rg-b3-nodes.name
 }
 
-resource "azurerm_subnet" "s-b3-vm1" {
+resource "azurerm_subnet" "s-b3-nodes" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.rg-b3-vm1.name
-  virtual_network_name = azurerm_virtual_network.vn-b3-vm1.name
+  resource_group_name  = azurerm_resource_group.rg-b3-nodes.name
+  virtual_network_name = azurerm_virtual_network.vn-b3-nodes.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "nic-b3-vm1" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.rg-b3-vm1.location
-  resource_group_name = azurerm_resource_group.rg-b3-vm1.name
+resource "azurerm_network_interface" "nic-b3-node1" {
+  name                = "nic-b3-node1"
+  location            = azurerm_resource_group.rg-b3-nodes.location
+  resource_group_name = azurerm_resource_group.rg-b3-nodes.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.s-b3-vm1.id
+    subnet_id                     = azurerm_subnet.s-b3-nodes.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.ip-b3-node1.id
+  }
+}
+
+resource "azurerm_network_interface" "nic-b3-node2" {
+  name                = "nic-b3-node2"
+  location            = azurerm_resource_group.rg-b3-nodes.location
+  resource_group_name = azurerm_resource_group.rg-b3-nodes.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.s-b3-nodes.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_linux_virtual_machine" "vm-b3-vm1" {
-  name                = "b3-vm1"
-  resource_group_name = azurerm_resource_group.rg-b3-vm1.name
-  location            = azurerm_resource_group.rg-b3-vm1.location
+resource "azurerm_public_ip" "ip-b3-node1" {
+  name                = "ip-b3-node1"
+  resource_group_name = azurerm_resource_group.rg-b3-nodes.name
+  location            = azurerm_resource_group.rg-b3-nodes.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_linux_virtual_machine" "vm-b3-node1" {
+  name                = "b3-node1"
+  resource_group_name = azurerm_resource_group.rg-b3-nodes.name
+  location            = azurerm_resource_group.rg-b3-nodes.location
   size                = "Standard_B1s"
-  admin_username      = "it4"
+  admin_username      = "gibbs"
   network_interface_ids = [
-    azurerm_network_interface.nic-b3-vm1.id,
+    azurerm_network_interface.nic-b3-node1.id,
   ]
 
   admin_ssh_key {
@@ -66,6 +86,34 @@ resource "azurerm_linux_virtual_machine" "vm-b3-vm1" {
     publisher = "canonical"
     offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm-b3-node2" {
+  name                = "b3-node2"
+  resource_group_name = azurerm_resource_group.rg-b3-nodes.name
+  location            = azurerm_resource_group.rg-b3-nodes.location
+  size                = "Standard_B1s"
+  admin_username      = "gibbs"
+  network_interface_ids = [
+    azurerm_network_interface.nic-b3-node2.id,
+  ]
+
+  admin_ssh_key {
+    username   = "gibbs"
+    public_key = file("id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Debian"
+    offer     = "debian-10"
+    sku       = "10"
     version   = "latest"
   }
 }
---
+++ tp3/readme.md
@@ -0,0 +1,13 @@
+```
+gibbs@b3-node1:~$ ping b3-node2
+PING b3-node2.120bvlftuafejjowv5nhgovpfc.bx.internal.cloudapp.net (10.0.2.5) 56(84) bytes of data.
+64 bytes from b3-node2.internal.cloudapp.net (10.0.2.5): icmp_seq=1 ttl=64 time=3.50 ms
+64 bytes from b3-node2.internal.cloudapp.net (10.0.2.5): icmp_seq=2 ttl=64 time=1.01 ms
+64 bytes from b3-node2.internal.cloudapp.net (10.0.2.5): icmp_seq=3 ttl=64 time=1.56 ms
+64 bytes from b3-node2.internal.cloudapp.net (10.0.2.5): icmp_seq=4 ttl=64 time=2.32 ms
+64 bytes from b3-node2.internal.cloudapp.net (10.0.2.5): icmp_seq=5 ttl=64 time=1.01 ms
+^C
+--- b3-node2.120bvlftuafejjowv5nhgovpfc.bx.internal.cloudapp.net ping statistics ---
+5 packets transmitted, 5 received, 0% packet loss, time 4007ms
+rtt min/avg/max/mdev = 1.007/1.878/3.495/0.940 ms
+```
